7dd0fd3381b45a1db7d511382e9751dfdd554c23
jCarlosDedica <juancarlos.torres@dedica.com.mx>
2022-07-19 04:28:02 -0500

    ejercicio 1 escribe y muestra tu nombre por consola

---
+++ exercice-1/Program.cs
@@ -0,0 +1,4 @@
+﻿//Escribe un programa que reciba tu nombre y lo escriba por consola.
+Console.WriteLine("Hola, bienvenido, escribe tu nombre");
+string nombre = Console.ReadLine();
+Console.WriteLine("Hola, " + nombre);
