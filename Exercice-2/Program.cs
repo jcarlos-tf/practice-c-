9619bb705e4a0351899a229cb1960a73fed26b4e
jCarlosDedica <juancarlos.torres@dedica.com.mx>
2022-07-19 04:31:48 -0500

    Ejercicio 2 pedir la hora y mostrarla por consola

---
+++ Exercice-2/Program.cs
@@ -0,0 +1,4 @@
+﻿//Escribe un programa que tome la hora y la escriba por pantalla.
+Console.WriteLine("Hola, bienvenido, escribe la hora en curso");
+string hora = Console.ReadLine();
+Console.WriteLine("Son las:, " + hora);
